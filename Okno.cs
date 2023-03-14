324bf671e2b53ee09b23b3ef533f7c66cbbdd3a0
micha <miso.bezo@centrum.sk>
2023-03-14 18:09:18 +0100

    Refaktor

--- Okno.cs
+++ Okno.cs
@@ -13,6 +13,10 @@ namespace Matrix3
         private int sirka = Console.WindowWidth;//30;
         private Linia[] stlpce;
 
+        private int curRow;
+        private int curColl;
+        private bool jePrvy;
+
         public Okno() 
         {
             Console.CursorVisible = false;
@@ -22,7 +26,6 @@ namespace Matrix3
             
             bool jeViditelna = false;
             
-
             for (int i = 0; i < sirka; i++)
             {
                 int nahoda = rand.Next(0, this.hustotaPrazdneho);
@@ -38,72 +41,87 @@ namespace Matrix3
         public void chod()
         {
             Random rand = new Random();
-            int curColl = 0;
-            int curRow = 0;
-            bool jePrvy = true;
+            this.curColl = 0;
+            this.curRow = 0;
+            this.jePrvy = true;
             while (true)
             {
+                this.Zapis();
+                this.CheckJeDlhy();
+                this.CheckJeDole();
+                this.Resetuj();
                 
-                if (this.stlpce[curColl].JeViditelna())
-                {
-                    this.stlpce[curColl].PridajKvapku();
-                    nastavKurzor(curRow, curColl);
-                    Console.Write(this.stlpce[curColl].DajKvapku(curRow, jePrvy).Drop);
-                } else
-                {
-                    nastavKurzor(curRow, curColl);
-                    Console.Write(" ");
-                }
-                /*
-                this.stlpce[curColl].PridajKvapku();
-                nastavKurzor(curRow, curColl);
-                Console.Write(this.stlpce[curColl].DajKvapku(curRow, jePrvy).Drop);
-                */
-
-                if (this.stlpce[curColl].VelkostLin() >= this.stlpce[curColl].DlzkaLinie)
-                {
-                    this.stlpce[curColl].ZursKvapku();
-                    nastavKurzor(curRow - (this.stlpce[curColl].DlzkaLinie - 1), curColl);
-                    Console.Write(" ");
-                    jePrvy = false;
-                }
-
-                if (curColl >= this.sirka - 2)
-                {
-                    curRow++;
-                    curColl = -1;
-                    Thread.Sleep(50);
-                }
-
-                if (curRow >= this.vyska - 2)
-                {
-                    Console.Clear();
-                    for (int i = 0; i < this.stlpce.Length; i++)
-                    {
-                        this.stlpce[i].ClearKvapkaList();
-                    }
-                    curRow = 0;
-                    curColl = -1;
-                    jePrvy = true;
-
-                    //Random rand = new Random();
-
-                    bool jeViditelna = false;
-
-                    for (int i = 0; i < sirka; i++)
-                    {
-                        int nahoda = rand.Next(0, this.hustotaPrazdneho);
-                        if (nahoda == 1)
-                            jeViditelna = true;
-                        else
-                            jeViditelna = false;
-                        Linia line = new Linia(2, jeViditelna);
-                        this.stlpce[i] = line;
-                    }
-                }
-
                 curColl++;
-                //Thread.Sleep(50);
+            }
+        }
+
+        private void Zapis()
+        {
+            if (this.stlpce[this.curColl].JeViditelna())
+            {
+                this.stlpce[this.curColl].PridajKvapku();
+                nastavKurzor(this.curRow, this.curColl);
+                Console.Write(this.stlpce[this.curColl].DajKvapku(this.curRow, this.jePrvy).Drop);
+            }
+            else
+            {
+                nastavKurzor(this.curRow, this.curColl);
+                Console.Write(" ");
+            }
+            /*
+            this.stlpce[curColl].PridajKvapku();
+            nastavKurzor(curRow, curColl);
+            Console.Write(this.stlpce[curColl].DajKvapku(curRow, jePrvy).Drop);
+            */
+        }
+
+        private void CheckJeDlhy()
+        {
+            if (this.stlpce[this.curColl].VelkostLin() >= this.stlpce[this.curColl].DlzkaLinie)
+            {
+                this.stlpce[this.curColl].ZursKvapku();
+                nastavKurzor(curRow - (this.stlpce[this.curColl].DlzkaLinie - 1), this.curColl);
+                Console.Write(" ");
+                this.jePrvy = false;
+            }
+        }
+
+        private void CheckJeDole()
+        {
+            if (this.curColl >= this.sirka - 2)
+            {
+                this.curRow++;
+                this.curColl = -1;
+                Thread.Sleep(50);
+            }
+        }
+
+        private void Resetuj()
+        {
+            Random rand = new Random();
+
+            if (this.curRow >= this.vyska - 2)
+            {
+                Console.Clear();
+                for (int i = 0; i < this.stlpce.Length; i++)
+                    this.stlpce[i].ClearKvapkaList();
+
+                this.curRow = 0;
+                this.curColl = -1;
+                this.jePrvy = true;
+
+                bool jeViditelna = false;
+
+                for (int i = 0; i < sirka; i++)
+                {
+                    int nahoda = rand.Next(0, this.hustotaPrazdneho);
+                    if (nahoda == 1)
+                        jeViditelna = true;
+                    else
+                        jeViditelna = false;
+                    Linia line = new Linia(2, jeViditelna);
+                    this.stlpce[i] = line;
+                }
             }
         }
 
@@ -111,7 +129,5 @@ namespace Matrix3
         {
             Console.SetCursorPosition(stlpec, riadok);
         }
-
-
     }
 }
